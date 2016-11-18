d3b5afaeee920ebed6363c9e8132e4a8005b340a
Tai Chen <mindlikewater@gmail.com>
2016-11-17 21:08:05 -0500

    Added methods for adding new images, getting images, updating image descriptions, etc., and deleting images

---
+++ app/Http/Controllers/ImageController.js
@@ -0,0 +1,42 @@
+'use strict'
+
+const Image = use("App/Model/Image")
+
+class ImageController {
+  * create (request, response) {
+    let newData = request.only('caption', 'url', 'description')
+
+    let newImage = yield Image.create(newData)
+    response.status(201).json(newImage)
+  }
+
+  * index (request, response) {
+    let imageList = yield Image.query().select("*")
+
+    response.json(imageList)
+  }
+
+  * update (request, response) {
+    let id = request.param('id')
+    let image = yield Image.findBy('id', id)
+    let update = request.only('caption', 'url', 'description')
+
+    image.fill(update)
+    yield image.save()
+    response.status(202).json(image)
+  }
+
+  // * update (request, response) {
+  //
+  // }
+
+  * delete (request, response) {
+  let id = request.param('id')
+
+  let image = yield Image.findBy('id', id)
+  yield image.delete()
+  response.status(202).json({ success: "Image successfully deleted" })
+  }
+}
+
+module.exports = ImageController
